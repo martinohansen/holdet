aa97430ba5abb5c41937cccfd2a68701c8fafa13
Martin Hansen <dontbeevilpls@gmail.com>
2023-03-16 13:56:38 +0100

    Advance to next round

--- v1.py
+++ v1.py
@@ -50,7 +50,7 @@ url = "https://api.holdet.dk/tournaments/422?appid=holdet"
 response = session.get(url)
 tournament = response.json()
 
-url = "https://api.holdet.dk/games/644/rounds/11/statistics?appid=holdet"
+url = "https://api.holdet.dk/games/644/rounds/12/statistics?appid=holdet"
 response = session.get(url)
 game = response.json()
 
@@ -162,7 +162,7 @@ class Baller:
         ballers team
         """
         matches = league["matches"]
-        next_round: int = 27
+        next_round: int = 28
         start_index: int = matches["firstUnplayedMatch"]["firstUnplayedMatchIndex"]
 
         closest_match = None
